d2b22d3b3ba5dd9ea67206c3bc2ee88c077f55a0
hyochan <106391445+rlagycks@users.noreply.github.com>
2025-09-14 13:32:17 +0900

    feat: mvp 추가 수정

--- bazaar_kiosk/urls.py
+++ bazaar_kiosk/urls.py
@@ -1,4 +1,4 @@
-#/bazaar_kiosk/urls.py
+# FILE: bazaar_kiosk/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
@@ -6,5 +6,5 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("orders/", include("orders.urls")),
-    path("", RedirectView.as_view(url="/orders/", permanent=False)),
+    path("", RedirectView.as_view(url="/orders/", permanent=False)),  # / → /orders/
 ]
